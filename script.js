79f9b28975bdd8ab949b4b0f7715381132af36d6
Arzu <46844871+thearzuyilmaz@users.noreply.github.com>
2024-09-19 19:53:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function dicing() {
+
+  var random1 = Math.floor(Math.random() * 6) + 1;
+  var random2 = Math.floor(Math.random() * 6) + 1;
+  var diceLink1 = "./images/dice" + random1 + ".png";
+  var diceLink2 = "./images/dice" + random2 + ".png";
+  document.querySelector(".img1").setAttribute("src", diceLink1);
+  document.querySelector(".img2").setAttribute("src", diceLink2);
+
+  if (random1 > random2){
+    document.querySelector("h1").innerHTML = "🏆 Player 1 Wins";
+  } else if (random2 > random1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🏆";
+  } else {
+    document.querySelector("h1").innerHTML = "Draw";
+  }
+
+  return [diceLink1, diceLink2];
+
+}
+
+window.onload = dicing;
+
+
+
